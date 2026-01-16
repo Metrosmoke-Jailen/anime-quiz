f6e39cbc996f58179a24ebbb157b017dc8040599
Jailen-Truitt <jailen.johnson-truitt@students.dominican.edu>
2026-01-15 19:14:10 -0800

    updated

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def quiz():
-    return render_template("index.html")
+    return render_template("anime_quiz.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
